59299843c1e623582f15483160b59c9d95c44b43
junebug07 <junseo2@ualberta.ca>
2025-10-24 16:11:24 -0600

    lab 7 complete

--- app/src/main/java/com/example/androiduitesting/MainActivity.java
+++ app/src/main/java/com/example/androiduitesting/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.androiduitesting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
 
         cityList.setAdapter(cityAdapter);
 
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String cityName = dataList.get(position);
+                Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                intent.putExtra("cityName", cityName);
+                startActivity(intent);
+            }
+        });
+
         final Button addButton = findViewById(R.id.button_add);
         addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -64,5 +77,6 @@ public class MainActivity extends AppCompatActivity {
                 cityAdapter.clear();
             }
         });
+
     }
 }
